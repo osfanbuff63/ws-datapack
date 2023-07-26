40dc1f02e136c883b70b0001469670c5e955897d
actions-user <action@github.com>
2023-07-26 23:11:07 +0000

    deploy site update from osfanbuff63: chore: deps: Update dependency jekyll-feed to v0.17.0 (#84)
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com> (d4a2a2b017de4d5dbe040e1e8031484b519f8f38)

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "First alpha release!",
+        "excerpt":"v0.1.0-alpha1 released!   The first alpha release for v0.1.0 has been released! It currently doesn’t do much, but is a start!  ","categories": [],
+        "tags": [],
+        "url": "/ws-datapack/2022/07/02/first-alpha.html",
+        "teaser": null
+      }]
